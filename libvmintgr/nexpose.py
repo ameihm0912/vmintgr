7403759303b78329d87c3aaa659ef83eaa340564
alm <alm@debian-dev2.internal>
2014-11-25 23:58:00 -0600

    Additional framework stuff

--- libvmintgr/nexpose.py
+++ libvmintgr/nexpose.py
@@ -1,12 +1,43 @@
 import sys
+import xml.etree.ElementTree as ET
 
 sys.path.append('../../pnexpose')
 
 import pnexpose
+import debug
 
 class nexpose_connector(object):
     def __init__(self, server, port, user, pw):
-        self._conn = pnexpose.nexposeClient(server, port, user, pw)
+        self.conn = pnexpose.nexposeClient(server, port, user, pw)
+        self.sitelist = {}
 
-def site_extraction(conf):
-    pass
+def site_extraction(scanner):
+    debug.printd('requesting site information')
+    sitedata = scanner.conn.site_listing()
+    root = ET.fromstring(sitedata)
+
+    for s in root:
+        siteinfo = {}
+        siteinfo['name'] = s.attrib['name']
+        siteinfo['id'] = s.attrib['id']
+        siteinfo['assets'] = []
+        scanner.sitelist[siteinfo['id']] = siteinfo
+    debug.printd('read %d sites' % len(scanner.sitelist))
+
+def asset_extraction(scanner):
+    for sid in scanner.sitelist.keys():
+        debug.printd('requesting devices for site %s (%s)' % \
+            (scanner.sitelist[sid]['id'], scanner.sitelist[sid]['name']))
+        devdata = scanner.conn.site_device_listing(sid)
+
+        root = ET.fromstring(devdata)
+        for s in root:
+            if s.tag != 'SiteDevices':
+                continue
+            siteid = s.attrib['site-id']
+            devlist = []
+            for d in s:
+                newdev = {}
+                newdev['id'] = d.attrib['id']
+                newdev['address'] = d.attrib['address']
+                scanner.sitelist[sid]['assets'].append(newdev)
--- vmintgr.py
+++ vmintgr.py
@@ -1,11 +1,56 @@
 #!/usr/bin/python2
 
 import sys
+import getopt
+
+sys.path.append('../pnexpose')
+
+import libvmintgr
+
+import pnexpose
+
+vmconfig = None
+debug = False
+scanner = None
+
+def usage():
+    sys.stdout.write('usage: vmintgr.py [-h] [-f path]\n')
+
+def wf_asset_grouping():
+    libvmintgr.printd('starting asset grouping workflow...')
+    libvmintgr.site_extraction(scanner)
+    libvmintgr.asset_extraction(scanner)
+    print scanner.conn.site_scan_history(1)
+    print scanner.conn.scan_statistics(1)
 
 def domain():
-	pass
+    global vmconfig
+    global debug
+    global scanner
+    confpath = None
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'df:h')
+    except getopt.GetoptError as e:
+        sys.stderr.write(str(e) + '\n')
+        usage()
+        sys.exit(1)
+    for o, a in opts:
+        if o == '-h':
+            usage()
+            sys.exit(0)
+        elif o == '-d':
+            libvmintgr.setdebugging(True)
+        elif o == '-f':
+            confpath = a
+
+    vmconfig = libvmintgr.VMConfig(confpath)
+    scanner = libvmintgr.nexpose_connector(vmconfig.vms_server, \
+        vmconfig.vms_port, vmconfig.vms_username, vmconfig.vms_password)
+
+    wf_asset_grouping()
 
 if __name__ == '__main__':
-	domain()
+    domain()
 
 sys.exit(0)
